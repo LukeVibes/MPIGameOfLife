5dac2d1849ca95be950325bff2e4f5ef0309872b
user <working54321@gmail.com>
2018-11-29 15:51:03 -0500

    Added rich comments to code, as well as added printing final game of life board to output.txt

--- main.cpp
+++ main.cpp
@@ -11,11 +11,18 @@
 
 using namespace std;
 
-//1. REMEBER TO TAKE A LOOK AT MYSUBMATRIX--> N/P1  N/P2 RAHTHER THAN MY_N
-//2. make 2d arrays int 1d
-//3. to declare nieghbor arrays somewhere (neighborEdges, recvNeighborEdges) down there you use p......why? 
-//4. improve print functions
-//5. ad p1=1 p2=1 functionality to fill_edges
+
+//. Have all values send to Proc-0                 14:30 DONE
+//. clean up code										 DONE
+//. Have Proc-0 print to file final array          16:15 DONE
+//. Add runttime timer							   16:45
+//. improve print functions                        17:30
+//. GET PROPER EDGE SENDING!                       22:30
+
+
+//. add p1=1 p2=1 functionality to fill_edges
+//. test with p1 != p2 values
+//. test with N=odd values
 
 
 
@@ -29,8 +36,8 @@ int *continousSubMatrix;
 int *temp_contiousSubMatrix;  
 int *neighborEdges; 
 int *recvNeighborEdges;      
-int *finalMatrixSend;
-int *recvFinalMatrix;
+int *sendFinal;
+int *recvFinal;
 int *printArr;
 
 ///Debug Variables
@@ -39,6 +46,7 @@ bool debug = true;
 ///Global Consts
 int OUTOFBOUNDSVALUE = 0;
 
+///bigger: used for simple max calculations
 int bigger(int a, int b){
 	int r = -1;
 	if (a >= b){
@@ -50,6 +58,7 @@ int bigger(int a, int b){
 	return r;
 }
 
+///declareGlobalMatrix: simple malloc on gobalmatrix dataset
 void declareGlobalMatrix(int n) {
 	
 	globalmatrix = (int **)malloc(sizeof(int *) * n); 
@@ -59,6 +68,7 @@ void declareGlobalMatrix(int n) {
     }
 }
 
+///declareMySubMatrix: simple malloc on mySubMatrix and continousSubMatrix
 void declareMySubMatrix(int n, int p1, int p2) {
 	int my_n = (n*n)/(p1*p2);
 	
@@ -71,6 +81,8 @@ void declareMySubMatrix(int n, int p1, int p2) {
     continousSubMatrix = (int *)malloc(sizeof(int *) * my_n);
 }
 
+
+///declareSubMatrices; simple malloc on submatrices and continoussubmatrices
 void declareSubMatrices(int n, int p1, int p2){
 	
 	submatrices = (int ****)malloc(sizeof(int ***) * p1);
@@ -90,6 +102,8 @@ void declareSubMatrices(int n, int p1, int p2){
 	continousSubMatrices = (int *)malloc(sizeof(int *) * (n*n));
 }
 
+
+///fileToMatrix: turns input.txt into gobalmatrix
 void fileToMatrix(string file, int n) {
 		
 		ifstream inputFile;
@@ -115,6 +129,8 @@ void fileToMatrix(string file, int n) {
 		inputFile.close();
 }
 
+
+///printGMatrix: simply prints gobalmatrix
 void printGMatrix(int n){
 		
 		for (int i = 0; i < n; i++) {
@@ -126,6 +142,9 @@ void printGMatrix(int n){
 
 }
 
+
+///matrixDivider: turns the 2D globalmatrix into the 4D submatrices and then,
+///               turns submtrices into the 2D contionussubmatrices 
 void matrixDivider(int p1, int p2, int n){
 	int subi = 0;
 	int subj = 0;
@@ -175,6 +194,60 @@ void matrixDivider(int p1, int p2, int n){
 	
 }
 
+
+///printFinaltoFile: simply prints recvFinal to output.txt, along with runntime
+void printFinaltoFile(int N, int p1, int p2){
+				int rowlength = N/p2;
+				int submtrxSize = (N/p1) * (N/p2);  
+			
+				///File Streaming Set-up
+				ofstream outdata;
+				outdata.open("output.txt", ofstream::out | ofstream::trunc); 
+				if( !outdata ) {
+				  cout << "FILE OUTPUTTING ERROR" << endl;
+				  exit(1);
+				}
+				
+				for(int i=0; i<N; i++){
+					for(int j=0; j<N; j++){
+						if(i<N/p1){
+							if(j<N/p2){
+								outdata << recvFinal[ (rowlength*i) + j ] << " ";
+							}
+							
+							else if(j>=N/p2){
+								outdata << recvFinal[ (rowlength*i) + submtrxSize + (j-(N/p2)) ] << " ";
+							}
+						}
+						
+						
+						else if(i>=N/p1){
+							if(j<N/p2){
+								outdata << recvFinal[ (rowlength*(i-N/p1)) + (p1*submtrxSize) + j ] << " ";
+							}
+							
+							else if(j>=N/p2){
+								outdata << recvFinal[ (rowlength*(i-N/p1)) + (p1*submtrxSize) + submtrxSize + (j-(N/p2)) ] << " ";
+							}
+						}
+					
+					}
+					outdata << endl;
+				}
+				outdata << endl;
+				
+				
+				///File Stream Wrap-up
+				outdata.close()
+				
+				
+			
+
+}
+
+
+///sumOfNghbrs: given and (x,y) from mySubMatrix, gives the sum of the surrounding
+///             8 values, utiliing the neighbors edge values aswell
 int sumOfNghbrs(int rank, int p, int x, int y, int n1, int n2, int p1, int p2){
 
 	int i = x-1;
@@ -203,7 +276,7 @@ int sumOfNghbrs(int rank, int p, int x, int y, int n1, int n2, int p1, int p2){
 
 	bool printcond = false;
 	
-	if(rank == 1 and x==4 and y==0){printcond = true;}
+	if(rank == 5 and x==44 and y==44){printcond = true;}
 
 	int index;
 	int jndex;
@@ -395,6 +468,9 @@ int sumOfNghbrs(int rank, int p, int x, int y, int n1, int n2, int p1, int p2){
 	return sum;
 }
 
+
+///gameOfLifeRules: using the game of life rules decided whether to kill or birth or
+///                 keep alive values in mySubMatrix
 void gameOfLifeRules(int rank, int a, int b, int p1, int p2, int n){
 	
 	int sum=0;
@@ -404,7 +480,7 @@ void gameOfLifeRules(int rank, int a, int b, int p1, int p2, int n){
 			
 			sum = sumOfNghbrs(rank, p1*p2, i, j, a, b, p1, p2);
 			if(rank == 1){
-						cout << "[" << i << "," << j << "]   sum->" << sum <<  endl;
+						//cout << "[" << i << "," << j << "]   sum->" << sum <<  endl;
 						
 
 					}
@@ -438,6 +514,8 @@ void gameOfLifeRules(int rank, int a, int b, int p1, int p2, int n){
 	//clear temp array?
 }
 
+
+///twoDSubMatrix: simply turns the 1D contiousSubMatrix to the 2D mySubMatrix
 int twoDSubMatrix(int n, int p1, int p2){
 	int count = 0;
 	
@@ -449,6 +527,7 @@ int twoDSubMatrix(int n, int p1, int p2){
 	}
 }
 
+///freeAll; simmply frees all malloc-ed datasets
 void freeAll(int rank, int n, int p1, int p2){
 	
 	if(rank == 0){
@@ -486,11 +565,13 @@ void freeAll(int rank, int n, int p1, int p2){
 	free(temp_contiousSubMatrix);
 	free(recvNeighborEdges);
 	
-	free(finalMatrixSend);
-	free(recvFinalMatrix);
+	free(sendFinal);
+	free(recvFinal);
 	free(printArr);
 }
 
+
+///fill_neighborEdges: populates neighborEdges with the appropraite edge values
 void fill_neighborEdges(int rank, int p, int n1, int n2, int p1, int p2){
 	///n1= (((N*N)/(p1*p2))/(N/p1))----> N/p1
 	///n2= (((N*N)/(p1*p2))/(N/p2))----> N/p2
@@ -518,12 +599,6 @@ void fill_neighborEdges(int rank, int p, int n1, int n2, int p1, int p2){
 						}
 						else{
 							neighborEdges[(abs(rank-1)* edgelength)+i] = mySubMatrix[i][j];
-							
-							if(i==(n1-1)){
-								for(int i=0; i<p; i++){
-									neighborEdges[
-								}
-							}
 						}
 					
 				}
@@ -577,6 +652,8 @@ void fill_neighborEdges(int rank, int p, int n1, int n2, int p1, int p2){
 	
 }
 
+///updateSubMatrix: simply updates the 2D mySubMatrix with the value from the
+///                 1D temp_continousSubMatrix
 void updateSubMatrix(int rank, int n, int p1, int p2){
 	int count = 0;
 	
@@ -591,6 +668,7 @@ void updateSubMatrix(int rank, int n, int p1, int p2){
 		}
 	}
 }
+
 
 int main(int argc, char *argv[])
 {
@@ -610,8 +688,8 @@ int main(int argc, char *argv[])
 	int k;      /// k: number of evolutoins
 	int N;      /// N: boardgame is size NxN
 	int m;      /// m: at each m-th step proc-0 collects subrects from 
-			  ///    other procs and prints the current configuration
-			  ///    into the output file.
+				///    other procs and prints the current configuration
+				///    into the output file.
  
  
 	///Inital Bcast Variables
@@ -623,8 +701,8 @@ int main(int argc, char *argv[])
 		N = 10;
 		p1 = 2;
 		p2 = 2;
-		k  = 100;
-		m  = 25;
+		k  = 4;   ////Should be: 100
+		m  = 2;   ////Should be: 25
 		
 		sendbuf[0] = N;
 		sendbuf[1] = p1;
@@ -662,167 +740,127 @@ int main(int argc, char *argv[])
 //Step 2: Subdivide Matrix
 	if (rank == 0){
 		matrixDivider(p1, p2, N);
-		
-		if(debug==true){
-			for(int i=0; i<5; i++){
-				for(int j=0; j<5; j++){
-					cout << submatrices[0][1][i][j];
-				}
-				cout << endl<<endl;;
-			}
-		}
 	}
 	
-	
-    
-   
  //Step 3: Send out SubMatrices
 	if (MPI_Scatter(continousSubMatrices, ((N*N)/(p1*p2)), MPI_INT,
 					continousSubMatrix, ((N*N)/(p1*p2)), MPI_INT,
 					ROOT, MPI_COMM_WORLD) != MPI_SUCCESS){
 					cout << "----------SCATTER ERROR UH-OH---------" << endl;
-				}
+	}
    
     
  //Step 4: Conduct k evoluntionary steps in SYNC
+	///Bring continous matrix back to 2D Form (for convience)
 	twoDSubMatrix(N, p1, p2);
 	int edgelength = bigger(N/p1, N/p2);
-  
+	
+	///Datasets needed to communicate Neighbors Edges to each other
 	temp_contiousSubMatrix = (int *)malloc(sizeof(int *) * ((N/p1) * (N/p2)));  ///traverse each i-> (+N/p2)
-  
-	///neighborEdges = (int *)malloc(sizeof(int *) * (p1*p2)*((2*(N/p1)) + (2*(N/p2))));
-	///recvNeighborEdges = (int *)malloc(sizeof(int *) * (p1*p2)*(((N/p1)) + ((N/p2)))); ///made changes, use to look like one above
 	recvNeighborEdges = (int *)malloc(sizeof(int *) * (p1*p2) * edgelength);  
 	neighborEdges = (int *)malloc(sizeof(int *) * (p1*p2) * edgelength);  
-	finalMatrixSend = (int *)malloc(sizeof(int *) * (N*N));
-	recvFinalMatrix = (int *)malloc(sizeof(int *) * (N*N));
-	printArr		= (int *)malloc(sizeof(int *) * (N*N));
 	
-	///recvNeighborBoard = (int *)malloc(sizeof(int *) 
+	///Datasets needed for printing final matrix to output.txt
+	sendFinal		= (int *)malloc(sizeof(int *) * (N*N));
+	recvFinal 		= (int *)malloc(sizeof(int *) * (N*N));
+
+	///Initalize Edge Dataset to avoid errors (and for testing convience)
 	for (int i=0; i<(p * edgelength); i++){
 		recvNeighborEdges[i] = -1;
 	}
 	
-	
-	if(rank == 1){
-			cout << endl;
-			for(int i = 0; i<(N/p1); i++){
-				for(int j = 0; j<(N/p2); j++){
-					cout << mySubMatrix[i][j] << " ";
-				}
-				cout << endl;
-			}
+	///Evolve Game Of Life board!
+	for(int w=0; w<k; w++){
 		
-		}
-  
- 
-	for(int w=0; w<3; w++){
+		///Collect processors submatrix edge values
 		fill_neighborEdges(rank, p, N/p1, N/p2, p1, p2);
 		
-		/*
-		if(rank ==0){
-
-			cout << "sendNghbrEdges:  [" ;
-			
-			
-			for(int i=0; i<p*edgelength; i++){
-				if(i%5==0){cout << " | ";}
-				cout << neighborEdges[i] << ", ";
-			}
-			cout << "] " << endl;
-		}
-		* */
-		
+		///Begin with communicating processors with neighbors edge values
 		MPI_Alltoall(neighborEdges, edgelength, MPI_INT,
 					 recvNeighborEdges, edgelength, MPI_INT, 
 					 MPI_COMM_WORLD);
 		
-		
-		if(rank ==0){
-
-			/*
-			cout << "recvNghbrEdges:  [" ;
-			
-			
-			for(int i=0; i<p*edgelength; i++){
-				if(i%5==0){cout << " | ";}
-				cout << recvNeighborEdges[i] << ", ";
-			}
-			cout << "] " << endl<<endl;
-			*/ 
-		}
-		
-		///gameOfLifeRules(rank, ((N*N)/(p1*p2))/(N/p1), ((N*N)/(p1*p2))/(N/p2), p1, p2, N);
-		
+		///Conduct Game Of Life Rules onto processors submatrix
 		gameOfLifeRules(rank, (N/p1), (N/p2), p1, p2, N);
 		updateSubMatrix(rank, N, p1, p2);
 		
-		if(rank == 1){
-			cout << endl;
-			for(int i = 0; i<(N/p1); i++){
-				for(int j = 0; j<(N/p2); j++){
-					cout << mySubMatrix[i][j] << " ";
-				}
-				cout << endl;
-			}
-			
-		}
-		
-		
-		
-//Step 5: Send each processors board to Processor-0 to display, every m cycles
-/*
+//Step 5: Send each processors board to Processor-0 to display, every m cyclesS
 		if(((w+1) % m)==0){
-			///Prep finalMatrixSend
+			///Populate sendFinal with processors subMatrix values
+			int rowlength = N/p2;
 			for(int i = 0; i<N/p1; i++){
 				for(int j=0; j<N/p2; j++){
 				
-					finalMatrixSend[(i*(N/p2)) + j] = mySubMatrix[i][j]; ///Each Proc puts it in location 0, so only Proc-0 gets data sent to i B^)
+					/// Each processor will save data in the "Proc-0 section", that way
+					/// Proc-0 gets is the only one recvieving values (transpose logic).
+					sendFinal[(i*rowlength) + j] = mySubMatrix[i][j];
 				}
 			}
 			
-			///MPI_Gather?
-			MPI_Alltoall(finalMatrixSend, ((N/p1) * (N/p2)), MPI_INT,
-						 recvFinalMatrix, ((N/p1) * (N/p2)), MPI_INT,
+			///Send values to Proc-0
+			MPI_Alltoall(sendFinal, ((N/p1) * (N/p2)), MPI_INT,
+						 recvFinal, ((N/p1) * (N/p2)), MPI_INT,
 						 MPI_COMM_WORLD);
-					 
+			
+			
+			///Print final matrix to output.txt
 			if(rank==0){
-				///Print RecvFinalMatrix
-				///HARDCODED FOR P1*P2=4!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-				for(int subV=0; subV<(p1*p2); subV++){
-					for(int i = 0; i < N/p1; i++){
-						cout << (subV * (N/p2)) + i << " ";
-						printArr[(subV * (N/p2)) + i] = recvFinalMatrix[(subV*((N/p1) * (N/p2)))];
+				printFinaltoFile(N, p1, p2);	
+			}
+			
+			////SubMatrix by SubMatrix print for testing
+			if(1==0){
+				MPI_Barrier(MPI_COMM_WORLD);
+				if(rank == 0){
+					cout << "Rank 0 mySubMatrix" << endl;
+					for(int i = 0; i<(N/p1); i++){
+						for(int j = 0; j<(N/p2); j++){
+							cout << sendFinal[(i*rowlength) + j] << " ";
+						}
+						cout << endl;
 					}
-					cout << endl;
-				}
-				cout << endl;
-				
-				for(int i=0; i<N*N; i++){
 					
-					cout << printArr[i] << " ";
-					if(((i+1) % N) == 0){ cout << endl; }
 				}
 				
-				cout << endl << endl;
+				MPI_Barrier(MPI_COMM_WORLD);
+				if(rank == 1){
+					cout << "Rank 1 mySubMatrix" << endl;
+					for(int i = 0; i<(N/p1); i++){
+						for(int j = 0; j<(N/p2); j++){
+							cout << sendFinal[(i*rowlength) + j] << " ";
+						}
+						cout << endl;
+					}
+					
+				}
+				
+				MPI_Barrier(MPI_COMM_WORLD);
+				if(rank == 2){
+					cout << "Rank 2 mySubMatrix" << endl;
+					for(int i = 0; i<(N/p1); i++){
+						for(int j = 0; j<(N/p2); j++){
+							cout << sendFinal[(i*rowlength) + j] << " ";
+						}
+						cout << endl;
+					}
+				}
+				
+				MPI_Barrier(MPI_COMM_WORLD);
+				if(rank == 3){
+					cout << "Rank 3 mySubMatrix" << endl;
+					for(int i = 0; i<(N/p1); i++){
+						for(int j = 0; j<(N/p2); j++){
+							cout << sendFinal[(i*rowlength) + j] << " ";
+						}
+						cout << endl;
+					}
+				}
+				
 			}
 			
 		}
-		* */
-		
 	}
 	
-
-	
-	/// Proc-1 [____, right wall, bottom wall, _______]
-	/// Proc-2 [left wall, _____, _______, bottom wall]
-	/// Proc-3 [upper wall, _____, ______, right wall ]
-	/// Proc-4 [_______, upperwall, left wall,________]
-	
-
-
-
-
 	///Wrap-UP
 	MPI::Finalize();
 	freeAll(rank, N, p1, p2);
